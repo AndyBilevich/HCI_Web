0c54773d0865f44fca3dbea531aa5f9b3409d543
Matias S. Baiges <mbaiges@itba.edu.ar>
2020-04-25 18:35:53 -0300

    added nav drawer and nav bar, routed pages (we worked all together)

--- intellifox/src/App.vue
+++ intellifox/src/App.vue
@@ -1,24 +1,130 @@
+  
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cards-heart</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Favourites</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Homes</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-img src="./assets/roomslogo.png" contain height="30px" ></v-img>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Rooms</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-img src="./assets/deviceslogo.png" contain height="30px" ></v-img>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Devices</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-img src="./assets/routineslogo.png" contain height="30px" ></v-img>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Routines</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      clipped-left
     >
+      <v-row align="center" justify="center">
+          <v-col>
+            <v-row align="center" justify="left">
+              <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+              <v-toolbar-title>
+                <v-img src="./assets/logo_web.png" contain></v-img>
+              </v-toolbar-title>
+            </v-row>
+          </v-col>
+          <v-col align="center" justify="left">
+            <v-text-field
+              placeholder="Search..."
+              solo
+              clearable
+              append-icon="mdi-magnify"
+              color="white"
+              hide-details
+            />
+          </v-col>
+          <v-col/>
+      </v-row>
     </v-app-bar>
 
-    <v-content>
-      <p>Hello World!</p>
+    <v-card width="60">
+    <v-toolbar flat color="primary" dark>
+    <v-toolbar-title>User Profile</v-toolbar-title>
+          </v-toolbar>
+          <v-tabs vertical>
+        <v-tab>
+          <v-icon left>mdi-account</v-icon>
+        </v-tab>
+        <v-tab>
+          <v-icon left>mdi-lock</v-icon>
+        </v-tab>
+        <v-tab>
+          <v-icon left>mdi-access-point</v-icon>
+        </v-tab>
+        <v-tab>
+          <v-icon left>mdi-access-point</v-icon>
+        </v-tab>
+        <v-tab>
+          <v-icon left>mdi-access-point</v-icon>
+        </v-tab>
+
+      </v-tabs>
+    </v-card>
+
+    <v-content align="center">
+    
+      <router-view/>
+      
     </v-content>
+  
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
--- intellifox/src/plugins/vuetify.js
+++ intellifox/src/plugins/vuetify.js
@@ -4,4 +4,28 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+      themes: {
+        dark: {
+          grey900: '#121212',
+          greyB: '#363636',
+          grey700: '#505050',
+          grey600: '#636363',
+          grey500: '#8A8A8A',
+          grey400: '#ABABAB',
+          grey300: '#D0D0D0',
+          grey200: '#E2E2E2',
+          grey100: '#EEEEEE',
+          purple700: '#803BE0',
+          purple500: '#994AF1',
+          purple300: '#BB86FC',
+          purple100: '#E2CDFD',
+          red700: '#CC3535',
+          red500: '#ED483D',
+          red300: '#DF7576',
+          red100: '#FCCDD3',
+          error: '#b71c1c'
+        }
+      }    
+    }
 });
