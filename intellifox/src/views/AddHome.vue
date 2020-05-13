9a93c994741c409106343e1947e71ba66307493e
Gabriel Margossian <gmargossian@itba.edu.ar>
2020-05-12 23:31:27 -0300

    AddDevice&Home

--- intellifox/src/views/AddHome.vue
+++ intellifox/src/views/AddHome.vue
@@ -1,34 +1,52 @@
 <template>
   <div>
     <h1 class="text-left">Add House</h1>
+   
+      <v-row>
+        <v-col cols="1"></v-col>
+
+        <v-col cols="4">
+          <v-text-field
+            label="Name:"
+            filled
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="1"></v-col>
+
+        <v-col cols="4">
+          <v-text-field
+            label="Description (optional):"
+            filled
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+
+      <v-row>
+        <v-col cols="1"></v-col>        
+        <v-col cols="4">
+          <v-text-field
+            label="Pin (optional):"
+            filled
+          ></v-text-field>
+        </v-col>
+      </v-row>
     
-    <v-col cols="12" sm="6" md="3">
-        <v-text-field
-        label="Name"
-      ></v-text-field>
-    </v-col>
 
-    <v-col cols="12" sm="6" md="3">
-        <v-text-field
-        label="Description (optional)"
-      ></v-text-field>
-    </v-col>
-
-    <v-col cols="12" sm="6" md="3">
-        <v-text-field
-        label="Pin (optional)"
-      ></v-text-field>
-    </v-col>
+    
+      <v-row justify="end" align="end">
+        <v-col cols="8"></v-col>
+        <v-btn class="ma-2" outlined large color="primary">Cancel</v-btn>
+        <div class="my-2">
+            <v-btn depressed large color="primary">Save</v-btn>
+        </div>
+      </v-row>
+    
   </div>
 </template>
 
 
 <style scoped>
-.homes {
-  padding: 10,
-}
-.card {
-  margin: 20;
-  cursor: pointer;
-}
+   
 </style>
